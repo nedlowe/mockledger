d63dabbc6a06bbdf8b51dee2b56294a923bc8f7b
EC2 Default User <ec2-user@ip-172-31-14-222.ap-southeast-1.compute.internal>
2019-03-27 14:28:49 +0000

    Added more API functionality

--- mockledger-backend/app.py
+++ mockledger-backend/app.py
@@ -1,7 +1,10 @@
 import boto3
 from boto3.dynamodb.conditions import Key
 from chalice import Chalice, BadRequestError
+import requests
 
+import calendar
+from datetime import date
 from dateutil.parser import parse
 from decimal import Decimal
 import logging
@@ -16,17 +19,25 @@ charges = dynamodb.Table('ledger_charges')
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 app.debug = True
-
+balance_api = 'INSERT API CALL HERE'
 
 @app.route('/ledgers/{customer_id}', methods=['POST'], cors=True)
 def create_ledger(customer_id):
     # Random number - this obviously is not for real usage!
     account_id = str(random.randint(1, 999999999))
+    
+    json_body = app.current_request.json_body
+    if not json_body:
+        raise BadRequestError("Missing body")
+    main_ledger = json_body.get('main_ledger')
+    charge_ledger = json_body.get('charge_ledger')
     # insert into DynamoDB
     ledgers.put_item(
         Item={
             'customer_id': customer_id,
-            'account_id': account_id
+            'account_id': account_id,
+            'main_ledger': main_ledger,
+            'charge_ledger': charge_ledger
             }
     )
     return {"account_id": account_id}
@@ -35,12 +46,13 @@ def create_ledger(customer_id):
 @app.route('/balances/{account_id}', methods=['GET'], cors=True)
 def get_monthly_balances(account_id):
     params = app.current_request.query_params
-    month = params.get('month')
-    if not month:
+    year_month = params.get('month')
+    if not year_month:
         raise BadRequestError("Missing month query param")
     # This is lame - do it properly
-    start_date = month + '-01'
-    end_date = month + '-31'
+    start_date = year_month + '-01'
+    year, month = year_month.split('-')
+    end_date = year_month + '-' + str(calendar.monthrange(int(year), int(month))[1])
     response = balances.query(
         KeyConditionExpression=Key('account_id').eq(account_id) & Key('ledger_date').between(start_date, end_date)
     )
@@ -49,16 +61,24 @@ def get_monthly_balances(account_id):
 
 @app.route('/balances/{account_id}/{ledger_date}', methods=['POST', 'GET'], cors=True)
 def proccess_balances(account_id, ledger_date):
+    logger.info("Processing Balance")
     # Get quantity from payload
     request = app.current_request
     if request.method == 'GET':
+        logger.info("Getting balance for date: %s", ledger_date)
         balance_quantity = get_balance(account_id=account_id, ledger_date=ledger_date)
         return balance_quantity
     else:
-        json_body = request.json_body
-        if not json_body:
-            raise BadRequestError("Missing balance quantity")
-        balance_quantity = Decimal(json_body.get('balance_quantity'))
+        logger.info("Posting new balance for date: %s", ledger_date)
+        if ledger_date != date.today().strftime("%Y-%m-%d"):
+            json_body = app.current_request.json_body
+            balance_quantity = Decimal(json_body.get('balance_quantity'))
+        else: 
+            # Remove hardcoded customer id
+            logger.info("Requesting latest balance from Ledger Operations")
+            details = requests.get(balance_api).json()
+            balance_quantity = Decimal(details['data']['primary_ledger_data']['amount']).quantize(Decimal("0.0001"))
+            logger.info("Latest balance: %s", balance_quantity)
         # TODO - some validation on the date
         # ledger_date = parse(ledger_date).strftime("%Y-%m-%d")
         insert_balance(account_id=account_id, ledger_date=ledger_date, balance_quantity=balance_quantity)
@@ -86,6 +106,15 @@ def insert_charge(account_id, ledger_date, charge_type, charge_value):
             }
     )
 
+def get_ledger_id(account_id, ledger_type='main_ledger'):
+    # THIS NO LONGER WORKS AFTER CHANGING THE DDB SCHEMA!
+    # TODO - FIX
+    ledger = ledgers.get_item(
+        Key={'account_id': account_id}
+    )
+    ledger_id = ledger['Item'][ledger_type]
+    return ledger_id
+
 def get_balance(account_id, ledger_date):
     balance = balances.get_item(
         Key={
@@ -103,9 +132,10 @@ def get_balance(account_id, ledger_date):
 def post_eod(account_id, ledger_date):
     balance = get_balance(account_id=account_id, ledger_date=ledger_date) 
     crediting_rate = Decimal('0.01')
-    crediting_amount = -1 * balance * crediting_rate
+    crediting_amount = balance * crediting_rate
     logger.info("Crediting Amount: %s", crediting_amount)
     insert_charge(account_id=account_id, ledger_date=ledger_date, charge_type='credit', charge_value=crediting_amount)
+    # Interledger transfer from singlife to charges
     return crediting_amount
 
 # Process EOD
@@ -115,13 +145,16 @@ def process_eom(account_id, ledger_month):
     # Get all charges for month
     # This is lame - do it properly
     start_date = ledger_month + '-01'
-    end_date = ledger_month + '-31'
+    year, month = ledger_month.split('-')
+    end_date = ledger_month + '-' + str(calendar.monthrange(int(year), int(month))[1])
     response = charges.query(
         KeyConditionExpression=Key('account_id').eq(account_id) & Key('ledger_date').between(start_date, end_date)
     )
+    # Sum up charges
+    total_quantity = -1 * sum([Decimal(item['charge_value']) for item in response['Items']])
     # If it's a POST, also write the result back
     if app.current_request.method == 'POST':
-        pass
+        insert_charge(account_id=account_id, ledger_date=end_date, charge_type='credit_settle', charge_value=total_quantity)
+        # Interledger transfer from charges to main
+    logger.info("Total Quantity is: %s", total_quantity)
     return response
-    # Sum up charges
-    # Move charge between ledgers
